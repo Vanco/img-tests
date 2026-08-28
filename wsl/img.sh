e57dc8423551baa85b14910166ac0c958ab6f6f8
Vanco <van.fan@outlook.com>
2026-08-28 10:54:29 +0800

    Update the moss repo url to kdl format.

--- wsl/img.sh
+++ wsl/img.sh
@@ -231,7 +231,7 @@ build() {
     export MOSS="moss -D ${SFSDIR} --cache ${CACHE}"
 
     echo ">>> Add volatile AerynOS repository to ${SFSDIR}/ ..."
-    time ${MOSS} repo add volatile https://build.aerynos.dev/stream/volatile/x86_64/stone.index || die_and_cleanup "Adding moss repo failed!"
+    time ${MOSS} repo add volatile https://build.aerynos.dev --root-index channel=main,version=stream/volatile || die_and_cleanup "Adding moss repo failed!"
 
     #echo ">>> Add local repo to ${SFSDIR}/ ..."
     #time ${MOSS} repo add local file:///home/ermo/.cache/local_repo/x86_64/stone.index -p10 || die_and_cleanup "Adding moss repo failed!"
